b97479c8ddc242cb9d8c706832384eae86cce9a6
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-07-21 10:52:06 -0700

    Fixed missing return on map.

--- src/ParticipantList.js
+++ src/ParticipantList.js
@@ -1,34 +1,38 @@
 import React from 'react';
 
-function ParticipantList(props) {
-  function checkStatus(inSession, onStage) {
-    if(!inSession) {
-      return 'offline';
-    }
-    else if(onStage) {
-      return 'on stage';
-    }
-    else {
-      return 'in session';
-    }
+function checkStatus(inSession, onStage) {
+  if(!inSession) {
+    return 'offline';
   }
+  else if(onStage) {
+    return 'on stage';
+  }
+  else {
+    return 'in session';
+  }
+}
 
+function ParticipantList(props) {
   return <div>
-    {props.participants.map(par => {
-      <section id={par.id} className="participant">
-        <img src={par.avatar} className="avatar" />
-        <div className="info">
-          <div className="name">{par.name}</div>
-          <div className="status">
-            <div className={par.inSession ? 'sessionOn' : 'sessionOff'}>&#11044;</div>
-            <div>
-              {checkStatus(par.inSession, par.onStage)}
-            </div>
-          </div>
-        </div>
-      </section>
-    })}  
-  </div>
+      {props.participants.map(par => {
+        return  <section id={par.id} className="participant">
+                  <img src={par.avatar} className="avatar" alt="Avatar" />
+                  <div className="info">
+                    <div className="name">
+                      {par.name}
+                    </div>
+                    <div className="status">
+                      <div className={par.inSession ? 'sessionOn' : 'sessionOff'}>
+                        &#11044;
+                      </div>
+                      <div>
+                        {checkStatus(par.inSession, par.onStage)}
+                      </div>
+                    </div>
+                  </div>
+                </section>
+  })}
+  </div>;
 }
 
 export default ParticipantList;
